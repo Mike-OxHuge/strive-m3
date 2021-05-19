c287b67501c945602ce69dfce6273112c96b0080
blade1995 <krnicola1995@gmail.com>
2021-05-19 16:56:48 +0200

    deleted primaryArray

--- mykola.js
+++ mykola.js
@@ -1,16 +1,19 @@
 
-const primaryArray = []
+
 
 window.onload = function () {
+ 
   const btn = document.querySelectorAll(".btn-group button:last-of-type");
   const svg= document.querySelectorAll(".card svg:first-of-type")
+  const img = document.querySelectorAll(".card img")
   const small = document.getElementsByTagName("small")
 
   for (let i = 0; i < btn.length; i++) {
     btn[i].innerText = " "  
     btn[i].innerText = "Hide";
     btn[i].addEventListener("click", () => {
-            svg[i].classList.add("d-none")
+
+            svg[i].remove()
     })
    
  }
