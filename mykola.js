15792c17e93aacfa3e3d6112a280770154f49c10
Mike Oxhuge <mikelitoris34@icloud.com>
2021-05-19 15:16:35 +0200

    commit on day3

--- mykola.js
+++ mykola.js
@@ -1,1 +1,8 @@
 console.log("test-mykola " + "200");
+window.onload = function () {
+  const btn = document.querySelectorAll(".btn-group button:last-of-type");
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].innerText = "жопа";
+  }
+  console.log(btn);
+};
