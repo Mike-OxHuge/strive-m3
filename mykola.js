873cc71723a30a77e65e8e7d9d453eb77e9a9d25
Mike Oxhuge <mikelitoris34@icloud.com>
2021-05-19 17:39:38 +0200

    commit on day3

--- mykola.js
+++ mykola.js
@@ -1,22 +1,27 @@
-
-
-
 window.onload = function () {
- 
+  const svg = document.querySelectorAll(".card svg:first-of-type");
+  const img = document.querySelectorAll(".card img");
+  const small = document.getElementsByTagName("small");
+
   const btn = document.querySelectorAll(".btn-group button:last-of-type");
-  const svg= document.querySelectorAll(".card svg:first-of-type")
-  const img = document.querySelectorAll(".card img")
-  const small = document.getElementsByTagName("small")
 
   for (let i = 0; i < btn.length; i++) {
-    btn[i].innerText = " "  
+    btn[i].innerText = " ";
     btn[i].innerText = "Hide";
     btn[i].addEventListener("click", () => {
-
-            svg[i].remove()
-    })
-   
- }
-}
+      svg[i].remove();
+      const pics = document.querySelectorAll(".img-fluid");
+      if (pics.length > 0) {
+        pics[i].remove();
+      }
+    });
+  }
+};
 //   console.log(btn);
 
+// for (let i = 0; i < pics.length; i++) {
+//   btn[i].addEventListener("click", () => {
+
+//
+//   });
+// }
