5f5c4256abae31f757a91b6d749f3e20ea66ba67
blade1995 <krnicola1995@gmail.com>
2021-05-19 15:48:00 +0200

    fixed the bug  was btn. but must be btn[i].

--- mykola.js
+++ mykola.js
@@ -3,10 +3,10 @@ window.onload = function () {
   const btn = document.querySelectorAll(".btn-group button:last-of-type");
   const svg = document.querySelectorAll(".album")
   for (let i = 0; i < btn.length; i++) {
-    btn[i].innerText =  ""  
+    btn[i].innerText = " "  
     btn[i].innerText = "Hide";
-    btn.addEventListener("click", () => {
-            album[i]. parentNode.removeChild("svg")
+    btn[i].addEventListener("click", () => {
+            album[i].parentNode.removeChild("svg")
     })
   }
 //   console.log(btn);
