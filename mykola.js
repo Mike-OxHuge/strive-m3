877c91e7d176a585a1926580cfcbcdc6b23b2f39
blade1995 <krnicola1995@gmail.com>
2021-05-19 15:41:02 +0200

    removed console.log for u Mike

--- mykola.js
+++ mykola.js
@@ -1,8 +1,14 @@
-console.log("test-mykola " + "200");
+
 window.onload = function () {
   const btn = document.querySelectorAll(".btn-group button:last-of-type");
+  const svg = document.querySelectorAll(".album")
   for (let i = 0; i < btn.length; i++) {
-    btn[i].innerText = "жопа";
+    btn[i].innerText =  ""  
+    btn[i].innerText = "Hide";
+    btn.addEventListener("click", () => {
+            album[i]. parentNode.removeChild("svg")
+    })
   }
-  console.log(btn);
+//   console.log(btn);
 };
+
