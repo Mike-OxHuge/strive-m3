75ecb84a58596f3c78a99650f6df6d1f9ab62ce9
Samuel Obeng <61053627+samlobeng@users.noreply.github.com>
2021-05-21 16:36:43 +0200

    hot fixes

--- search/app.js
+++ search/app.js
@@ -1,5 +1,8 @@
 let homeAlbums = []
 let cards = document.querySelector(".album-row")
+let artist = document.querySelector(".artist-row")
+let hidden = document.querySelector(".hidden")
+let sample = document.querySelector(".sample")
 window.onload = function(){
     fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=queen")
     .then(response => response.json())
@@ -28,7 +31,12 @@ function search(){
             console.log(albums);
             homeAlbums = albums;
             albums.data.forEach(album => {
-                
+                artist.innerHTML= `
+                <div class="artist-container">
+                <img src="${album.artist.picture_small}" class="rounded-circle img-fluid mt-3 ml-3" style="width: 100px; height: 100px;" alt="...">
+                <p><span class="artist-name ml-4 text-white" style = "font-size: large;">${album.artist.name}</span></p>
+            </div>`
+            
                 cards.innerHTML += `<div class="mb-4 text-center col-sm-6 col-md-4 col-lg-2">
                 <img class="img-fluid" src="${album.album.cover_big}"><h6 class="text-white album-title mt-1">${album.album.title}</h6></div>
                 <div class="mb-4 text-center">`
