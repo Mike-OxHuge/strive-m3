9b544bccd6728b6110bc2dff49c3bc5859eadd87
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-05-25 14:00:55 +0100

    commit on day7 -m excercise 1

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 // Ex1) Get and display, using an asynchronous pattern,
 // the users from: https://jsonplaceholder.typicode.com/users
+
+fetch("https://jsonplaceholder.typicode.com/users").then((resp)=> resp.json()).then((data) =>console.log(data))
