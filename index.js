312a7a789f81aa5f54916cc9336f626ebc59779a
Mike Oxhuge <mikelitoris34@icloud.com>
2021-05-25 15:12:03 +0200

    commit on day7

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Ex1) Get and display, using an asynchronous pattern,
 // the users from: https://jsonplaceholder.typicode.com/users
 
-fetch("https://jsonplaceholder.typicode.com/users").then((resp)=> resp.json()).then((data) =>console.log(data))
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((resp) => resp.json())
+  .then((data) => console.log(data));
+
+// Ex2) Create a dropdown (<select>) that allows the user to select between name,
+// username and email.
+// Create then a filter. When the user types in something, you should filter
+// the user based on the input and on the value of the select.
+// Es.: select on NAME. Filter input = Glenna, only user id number 9 should remain
+console.log(document.getElementById("inputGroupSelect01"));
