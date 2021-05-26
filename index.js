895dd843c63685c24dba2b0e9f425b521cf201a4
Mike Oxhuge <mikelitoris34@icloud.com>
2021-05-26 23:13:48 +0200

    commit on playground

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const gameStart = function () {
   <div class="input-group-prepend">
     <span class="input-group-text" id="basic-addon1">Start of range:</span>
   </div>
-  <input type="number" class="form-control" placeholder="0" id='startingPoint' required>
+  <input type="number" class="form-control" value="0" id='startingPoint' required>
 </div>
 
 <div class="input-group mb-3">
   <div class="input-group-prepend">
     <span class="input-group-text" id="basic-addon1">End of range:</span>
   </div>
-  <input type="number" class="form-control" placeholder="69" id='endingPoint' required>
+  <input type="number" class="form-control" value="69" id='endingPoint' required>
 </div>
 
 <button class="btn btn-primary" type="button" id='gameBegin'>Start the game</button>
